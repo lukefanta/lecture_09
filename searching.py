d02fd4ab797532d4dbe8fd7b2ed533b780235cd6
Lukas Fanta <240498@vutbr.cz>
2022-04-11 10:03:06 +0200

    pattern search

--- searching.py
+++ searching.py
@@ -32,11 +32,26 @@ def linear_search(sequence, number):
     }
     return slovnik
 
+def pattern_search(sequence, pattern):
+    position = set()
+    index = 0
+    while index < len(sequence) - len(pattern):
+        if sequence[index:index + len(pattern)] == pattern:
+            position.add(index)
+        index += 1
+    return position
+
+
+
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
     results = linear_search(sequential_data, 0)
     print(results)
+    dna = read_data("sequential.json", "dna_sequence")
+    print(pattern_search(dna, "ATA"))
 
 if __name__ == '__main__':
     main()
